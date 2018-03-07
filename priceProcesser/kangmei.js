d5ea47626315d79d149d9b1f5ac3b28f3c708927
kyoham@gmail.com <kyoham@gmail.com>
2018-03-07 15:55:19 +0800

    更新node_modules

--- priceProcesser/kangmei.js
+++ priceProcesser/kangmei.js
@@ -5,7 +5,7 @@
 
 module.exports = [{
     uri: 'http://m.kmzyw.com.cn/price/data/price_index_search2.jsp',
-    // uri: 'http://m.kmzyw.com.cn/jiage/jsp/data.jsp', 参数 name:名称，获取药品id
+    // uri: 'http://m.kmzyw.com.cn/jiage/jsp/data.jsp', post form 参数 name:名称，获取药品id
     // uri: 'http://m.kmzyw.com.cn/jiage/jsp/get_td_price.jsp',参数 表单 site:市场，drugid："id"，pages:0
     jquery: false,
     callback: function (error, res, done) {
--- test/tester.js
+++ test/tester.js
@@ -1,5 +1,52 @@
 /**
  * Created by bozhang on 2017/6/14.
  */
-const util = require("../util");
-console.log(util.decodeUTF8(undefined))
+"use strict";
+// const util = require("../util");
+// console.log(util.decodeUTF8(undefined));
+
+//
+// let promise1 = new Promise(function(resolve, reject) {
+//     console.log('Promise1');
+//     // reject(" Promise1 trigle error")
+//     resolve();
+// });
+// let promise2 = new Promise(function(resolve, reject) {
+//     console.log('Promise2');
+//     setTimeout(() => {
+//         reject(" Promise2 trigle error")
+//         // resolve();
+//     }, 3000);
+// });
+//
+// promise1.then(function() {
+//     console.log('promise1 Resolved.');
+//     return promise2;
+// }).then(() => {
+//     console.log("Then in 2nd step")
+// }).catch((error) => {
+//     console.log("error:" + error);
+// }).finally(() => {
+//     console.log("finally!!");
+// });
+//
+// console.log('Hi!');
+
+var arr = [1, [[2, 3], 4], [5, 6]];
+
+var flat = function* (a) {
+    var length = a.length;
+    for (var i = 0; i < length; i++) {
+        var item = a[i];
+        if (typeof item !== 'number') {
+            yield* flat(item);
+        } else {
+            yield item;
+        }
+    }
+};
+
+for (var f of flat(arr)) {
+    console.log(f);
+}
+// 1, 2, 3, 4, 5, 6
