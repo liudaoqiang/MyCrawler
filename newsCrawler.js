7edd9e63f5516371e1a2bba310f6d0a308e287c6
kyoham@gmail.com <kyoham@gmail.com>
2018-06-05 15:51:32 +0800

    调整抓取结果为每小时抓取

--- newsCrawler.js
+++ newsCrawler.js
@@ -65,7 +65,7 @@ const env = process.env.NODE_ENV ? process.env.NODE_ENV : 'local';
 if (env === 'local') {
     crawleInternetnews();
 } else {
-    schedule.scheduleJob('33 10 * * *', function () {
+    schedule.scheduleJob('33 * * * *', function () {
         crawleInternetnews();
     });
 }
