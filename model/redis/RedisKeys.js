1424181743f0e9d23e7aba86f1a2a8d00fc21055
kyoham@gmail.com <kyoham@gmail.com>
2018-06-21 23:56:06 +0800

    添加redis监听channel定义

--- model/redis/RedisKeys.js
+++ model/redis/RedisKeys.js
@@ -2,5 +2,8 @@
  * Created by bozhang on 2017/6/15.
  */
 module.exports = {
-    InternetPriceMedicineName : "price:internet:medicine_name"
+    InternetPriceMedicineName : "price:internet:medicine_name",
+
+    CHANNEL_INTERNET_NEWS: "channel:news:internet",
+    CHANNEL_INTERNET_CHANGED_PRICE: "channel:changed_prices:internet"
 };
